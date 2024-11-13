6767635bf84350ca9e33ad245b9d1fdfc659c21a
drewno026 <wiktor.ppniak@gmail.com>
2024-11-13 21:22:36 +0100

    Lab_04

---
+++ Lab_04/Lab_04/Album.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    internal class Album
+    {
+        public string AlbumTitle { get; set; }
+        public string AlbumAuthor { get; set; }
+        public int NumberOfSongs {  get; set; }
+        public int ReleaseYear { get; set; }
+
+        public static int HowManyAlbums { get; set; } = 0;
+
+        public Album()
+        {
+            AlbumTitle = "Unknown";
+            AlbumAuthor = "Unknown";
+            NumberOfSongs = 0;
+            ReleaseYear = 0;
+            HowManyAlbums++;
+        }
+        public Album(string albumTitle, string albumAuthor, int numberOfSongs, int releaseYear)
+        {
+            AlbumTitle = albumTitle;
+            AlbumAuthor = albumAuthor;
+            if (numberOfSongs < 0)  
+            {
+                throw new ArgumentException("Number of songs must be greater than zero");
+            }
+            else
+            {
+                NumberOfSongs = numberOfSongs;
+                int CurrentYear = DateTime.Now.Year;
+                if (releaseYear > CurrentYear || releaseYear <= (CurrentYear - 80))
+                {
+                    throw new ArgumentException("Year must be realistic");
+                }
+                else
+                {
+                    ReleaseYear = releaseYear;
+                }
+            }
+            HowManyAlbums++;
+        }
+        public Album(string albumTitle, int releaseYear) : this()
+        {
+            AlbumTitle = albumTitle;
+            int CurrentYear = DateTime.Now.Year;
+            if (releaseYear > CurrentYear || releaseYear <= (CurrentYear - 80))
+            {
+                throw new ArgumentException("Year must be realistic");
+            }
+            else
+            {
+                ReleaseYear = releaseYear;
+            }
+        }
+
+        public static void CleanAlbumCounter()
+        {
+            HowManyAlbums = 0;
+        }
+            public void ShowAlbumInfo()
+        {
+            Console.WriteLine("Album details: ");
+            Console.WriteLine(AlbumTitle);
+            Console.WriteLine(AlbumAuthor);
+            Console.WriteLine(NumberOfSongs);
+            Console.WriteLine(ReleaseYear + "\n");
+        }
+
+    }
+}
---
+++ Lab_04/Lab_04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Album album1 = new Album();
+            album1.ShowAlbumInfo();
+            Album album2 = new Album()
+            {
+                AlbumAuthor = "WP",
+                ReleaseYear = 2000
+            };
+            album2.ShowAlbumInfo();
+            Album album3 = new Album("WWWa", "JPII", 2137, 2004);
+            album3.ShowAlbumInfo();
+            Album album4 = new Album("Sssa", 2004);
+            album4.ShowAlbumInfo();
+            Album album5 = new Album("OP", "W", 5, 2014);
+            album5.ShowAlbumInfo();
+            Console.WriteLine($"Number of Albums {Album.HowManyAlbums}");
+            Album.CleanAlbumCounter();
+            Console.WriteLine($"Number of Albums after reset {Album.HowManyAlbums}");
+        }
+    }
+}
