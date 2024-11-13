e7f98aa95ade3059a6b63cf1bffec08ff84b7a39
drewno026 <wiktor.ppniak@gmail.com>
2024-11-13 22:40:54 +0100

    ZadanieDomowe_04

---
+++ Lab_04/ZadanieDomowe_04/ZadanieDomowe_04/MenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe_04
+{
+    internal class MenuItem
+    {
+        public string DishName { get; set; }
+        public double Price { get; set; }
+        public static double TotalOrder { get; private set; } = 0.0;
+    
+        public MenuItem(string dishName, double price)
+        {
+            DishName = dishName;
+            Price = price;
+            TotalOrder = TotalOrder + price;
+        }
+    
+        public MenuItem(MenuItem other)
+        {
+            DishName = other.DishName;
+            Price = other.Price;
+            TotalOrder = TotalOrder + other.Price;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{DishName}, Price: {Price:C}");
+        }
+
+    }
+}
---
+++ Lab_04/ZadanieDomowe_04/ZadanieDomowe_04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ZadanieDomowe_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuItem item1 = new MenuItem("Pizza", 25.50);
+            item1.DisplayInfo();
+            Console.WriteLine($"Total Order Value: {MenuItem.TotalOrder:C}");
+
+            MenuItem item2 = new MenuItem("Beer", 7.50);
+            item2.DisplayInfo();
+            Console.WriteLine($"Total Order Value: {MenuItem.TotalOrder:C}");
+
+
+            MenuItem item3 = new MenuItem(item2);
+            item3.DisplayInfo();
+            Console.WriteLine($"Total Order Value: {MenuItem.TotalOrder:C}");
+        }
+    }
+}
