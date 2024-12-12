450a31c895e3c91b3bb5ffbf335637c4e520f12a
drewno026 <wiktor.ppniak@gmail.com>
2024-12-13 00:29:42 +0100

    ZadanieDomowe_08

---
+++ Lab_08/Zadanie_08/Zadanie_08/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace B8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = ZadanieDomowe.Read();
+
+            //(1 pkt) Proszę znaleźć i wypisać na ekran wszystkie słowa składające się z dokładnie
+            //trzynastu liter.
+            // A 
+            Console.WriteLine("Podpunkt A:\n");
+            List<string> answerA = words.Where(word => word.Length == 13).ToList();
+            foreach (string word in answerA) Console.WriteLine(word);
+            Console.WriteLine();
+
+            //(2 pkt) Proszę zastąpić każde słowo w tekście jego pierwszą literą i wypisać na ekran
+            //dwadzieścia pierwszych rezultatów.
+            // B
+            Console.WriteLine("Podpunkt B:\n");
+            List<char> answerB = words.Select(word => word.First()).ToList();
+            for (int i = 0; i < 20; i++) Console.WriteLine(answerB[i]);
+            Console.WriteLine();
+
+            //(2 pkt) Proszę obliczyć średnią długość wszystkich słów w tekście, zakładając, że
+            //powtarzające się słowa liczymy tylko raz. Wynik należy podać jako liczbę
+            //zmiennoprzecinkową.
+            // E
+            Console.WriteLine("Podpunkt E:\n");
+            double answerE = words.Distinct().Average(word => word.Length);
+            Console.WriteLine("{0:0.0}", answerE);
+            Console.WriteLine();
+
+            //(2 pkt) Proszę sprawdzić, czy wszystkie słowa o długości większej niż dziesięć liter
+            //zawierają małą literę 'z'.Odpowiedź należy podać jako typ logiczny True/ False.
+            //Pomocna może okazać się metoda String.Contains.
+            // F
+            Console.WriteLine("Podpunkt F:\n");
+            bool answerF = words.All(word => word.Length <= 10 || word.Contains('z'));
+            Console.WriteLine(answerF);
+            Console.WriteLine();
+
+            //(3 pkt) Proszę obliczyć, ile jest wszystkich liter w tekście
+            // G
+            Console.WriteLine("Podpunkt G:\n");
+            int answerG = words.Sum(word => word.Length);
+            Console.WriteLine(answerG);
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ Lab_08/Zadanie_08/Zadanie_08/ZadanieDomowe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace B8
+{
+    class ZadanieDomowe
+    {
+        public static List<string> Read()
+        {
+            //tutaj nalezy zmienic sciezke do pliku
+            string filepath = @"C:\Users\wikto\Downloads\KsiegaPierwszaBezInterpunkcji.txt"; 
+
+            // wczytywanie
+            try
+            {
+                StreamReader sr = new StreamReader(filepath);
+                List<string> ans = new List<string>();
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    ans.AddRange(line.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                return ans;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Nie udalo sie wczytac pliku ze sciezki " + filepath + ": " + e.Message);
+                return new List<string>();
+            } 
+        }
+
+
+    }
+}
